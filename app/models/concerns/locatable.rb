69000ebc7333b4e9b24e8c98b5af811afab99401
Abhishek Sarkar <abhisheksarka@gmail.com>
2015-01-04 20:11:03 +0530

    getting the last known location

--- app/models/concerns/locatable.rb
+++ app/models/concerns/locatable.rb
@@ -3,6 +3,7 @@ module Locatable
 
   included do
     has_many :locations, as: :locatable
+    has_one :last_location, -> { order id: :desc }, class_name: 'Location', foreign_key: :locatable_id
   end
 
   module ClassMethods

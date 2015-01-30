06e87c206b08c3b15e4e1291fc720654dfb29a64
Abhishek Sarkar <abhisheksarka@gmail.com>
2015-01-30 23:47:30 +0530

    post service handles the creation of a default postable

--- app/services/post_service.rb
+++ app/services/post_service.rb
@@ -10,17 +10,18 @@ class PostService
 
   def create(post_params, auto_spread=true)
     @post_params = post_params
+    set_default_postable 
+    post = post_publishable.posts.new(post_params)
     
-    if is_postable_associated_to_an_existing_post?
-      post = Post.new
-      post.errors[:post] << "already associated to a postable"
+    if valid_postable?
+      post.save
+      # create the spread object
+      spread(post) if auto_spread and post.valid?
+      post
+    else
+      post.errors[:an_invalid_postable] << "cannot create a post object"
       post
     end
-
-    post = post_publishable.posts.create(post_params)
-    # create the spread object
-    spread(post) if auto_spread and post.valid?
-    post
   end
 
   def query
@@ -42,7 +43,34 @@ class PostService
     }
   end
 
+  # default postable is always text 
+  # create this automatically if a valid postable has not been found
+  def set_default_postable
+    if valid_postable?
+      return nil
+    end
+    default = PostText.create
+    post_params.merge!({ postable_id: default.id, postable_type: PostText.to_s })
+    default
+  end
+
+  # find the associated postable
+  def postable
+    postable_klass.find(post_params[:postable_id]) rescue nil
+  end
+
+  def postable_klass
+    post_params[:postable_type].constantize rescue nil
+  end
+
+  # check if the postable is already associated to a post
   def is_postable_associated_to_an_existing_post?
-    post_params[:postable_type].constantize.find(post_params[:postable_id]).post.present? rescue false
+    postable.post.present? rescue false
+  end
+
+  # a valid postable is one
+  # which is already created but is not associated to any post
+  def valid_postable?
+    postable.present? and !is_postable_associated_to_an_existing_post?
   end
 end

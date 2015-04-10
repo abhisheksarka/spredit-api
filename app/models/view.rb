4be484c1bff04a88aa62930e89df2b496c434086
Abhishek Sarkar <abhisheksarka@gmail.com>
2015-04-11 00:36:10 +0530

    updating the view counter cache

--- app/models/view.rb
+++ app/models/view.rb
@@ -4,4 +4,9 @@ class View < ActiveRecord::Base
   belongs_to :viewable, polymorphic: true
   belongs_to :view_publishable, polymorphic: true
   validates_with ViewValidator, on: :create
+  after_save :update_counter_cache
+  
+  def update_counter_cache
+    viewable.update(views_count: viewable.views.length)
+  end
 end

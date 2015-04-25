522297b6c340ce5ea2fd9d3cfb0033f7ee05b5b9
Abhishek Sarkar <abhisheksarka@gmail.com>
2015-04-25 11:59:49 +0530

    location max radius reduced to 15KM

--- app/models/configuration/location.rb
+++ app/models/configuration/location.rb
@@ -2,6 +2,6 @@ class Configuration::Location < ActiveRecord::Base
   belongs_to :configurable_location, polymorphic: true
 
   validates :radius, presence: true
-  validates :radius, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
+  validates :radius, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 15 }
   
 end
